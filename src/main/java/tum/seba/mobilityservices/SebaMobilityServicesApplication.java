5b4e7f336f1e034afa16a0cf1734ba23912c2989
Nektarios Machner <nektarios.machner@tum.de>
2020-12-11 11:46:37 +0100

    Exercise-Template pushed by Artemis

---
+++ src/main/java/tum/seba/mobilityservices/SebaMobilityServicesApplication.java
@@ -0,0 +1,22 @@
+package tum.seba.mobilityservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class SebaMobilityServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebaMobilityServicesApplication.class, args);
+	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void execCodeAfterStartup() {
+	
+		// Instantiate your classes here and output their content to the console by calling their toString() method
+		
+	}
+
+}
