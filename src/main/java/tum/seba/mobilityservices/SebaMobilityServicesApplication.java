8543b55feea7a7714a5bf8f918c7e59616f1cf06
Stratos Delis <t8220033@aueb.gr>
2024-11-24 19:48:06 +0200

    Started with the instantiations and the toStrings

--- src/main/java/tum/seba/mobilityservices/SebaMobilityServicesApplication.java
+++ src/main/java/tum/seba/mobilityservices/SebaMobilityServicesApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import tum.seba.mobilityservices.entity.*;
+import java.util.Date;
 
 @SpringBootApplication
 public class SebaMobilityServicesApplication {
@@ -16,7 +18,19 @@ public class SebaMobilityServicesApplication {
 	public void execCodeAfterStartup() {
 	
 		// Instantiate your classes here and output their content to the console by calling their toString() method
-		
+
+		//Instantiations
+		Rental rental = new Rental(new Date(2024-10-10), new Date(2024-10-12), Rental.Status.BOOKED);
+		Invoice invoice = new Invoice(50.0, true);
+		ServicePoint servicePoint = new ServicePoint("SP X", "StreetName", 99, "Muenchen");
+		Vehicle vehicle = new Vehicle("ManufacturerX", "ModelTypeX", new Date(2023-10-10), true);
+
+		//toString calls
+		System.out.println(rental);
+		System.out.println(invoice);
+		System.out.println(servicePoint);
+		System.out.println(vehicle);
 	}
+
 
 }
