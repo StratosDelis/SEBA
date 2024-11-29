86c32d91894a2e4853017bdab23aabcd45bd01b4
alkminisarigeorgiou <alkminisarigeorgiou@gmail.com>
2024-11-29 17:31:55 +0200

    Rental Service

--- src/main/java/tum/seba/mobilityservices/repository/RentalRepository.java
+++ src/main/java/tum/seba/mobilityservices/repository/RentalRepository.java
@@ -1,4 +1,10 @@
 package tum.seba.mobilityservices.repository;
 
-public class RentalRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tum.seba.mobilityservices.entity.Rental;
+
+@Repository
+public interface RentalRepository extends JpaRepository<Rental,Integer>{
+
 }
--- src/main/java/tum/seba/mobilityservices/service/RentalService.java
+++ src/main/java/tum/seba/mobilityservices/service/RentalService.java
@@ -1,4 +1,40 @@
 package tum.seba.mobilityservices.service;
 
-public class RentalService dd{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import tum.seba.mobilityservices.entity.Rental;
+import tum.seba.mobilityservices.repository.RentalRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RentalService  {
+    @Autowired
+    private RentalRepository rentalRepository;
+
+    //C : save a rental
+    public Rental saveRental(Rental rental){
+        return rentalRepository.save(rental);
+    }
+    //R: find all rentals
+    public List<Rental> findAll(){
+        return rentalRepository.findAll();
+    }
+    //R: find rental by id
+    public Rental findRentalbyId(int rentalid){
+         Optional<Rental> foundrental = rentalRepository.findById(rentalid);
+         return foundrental.orElse(null);
+    }
+    //D: delete rental by id
+    public void deleteRentalById(int rentalid){
+        try{
+            rentalRepository.deleteById(rentalid);
+        }catch(Exception e){
+            System.out.println("Unable to delete rental with id:"+rentalid);
+        }
+    }
+
+    
 }
