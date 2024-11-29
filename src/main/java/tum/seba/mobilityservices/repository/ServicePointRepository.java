ee458ad3f92b0209453263219cde14f76b6f395d
alkminisarigeorgiou <alkminisarigeorgiou@gmail.com>
2024-11-29 18:06:24 +0200

    Service Point Repository

--- src/main/java/tum/seba/mobilityservices/repository/ServicePointRepository.java
+++ src/main/java/tum/seba/mobilityservices/repository/ServicePointRepository.java
@@ -1,2 +1,8 @@
-package tum.seba.mobilityservices.repository;public class ServicePointRepository {
+package tum.seba.mobilityservices.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tum.seba.mobilityservices.entity.ServicePoint;
+
+public interface ServicePointRepository extends JpaRepository<ServicePoint,Integer> {
+
 }
