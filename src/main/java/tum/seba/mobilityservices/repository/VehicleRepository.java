be6f1cc17c8f0e628e673797ffc771defbceba29
Stratos Delis <stratosdelis21@gmail.com>
2024-12-03 00:52:16 +0200

    UserService class creation with basic CRUD Methods for Customer and Employee, @Repository annotations for repositories: Customer, Employee, Vehicle

--- src/main/java/tum/seba/mobilityservices/repository/VehicleRepository.java
+++ src/main/java/tum/seba/mobilityservices/repository/VehicleRepository.java
@@ -1,8 +1,9 @@
 package tum.seba.mobilityservices.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tum.seba.mobilityservices.entity.Vehicle;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
-    // baddieeeeee
 }
