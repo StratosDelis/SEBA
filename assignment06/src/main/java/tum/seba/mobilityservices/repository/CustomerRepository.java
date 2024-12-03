3212e7819a05eb7ed9089142741795484c36b44c
Stratos Delis <stratosdelis21@gmail.com>
2024-12-03 20:14:01 +0200

    Remove false version files

--- assignment06/src/main/java/tum/seba/mobilityservices/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package tum.seba.mobilityservices.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tum.seba.mobilityservices.entity.Customer;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
